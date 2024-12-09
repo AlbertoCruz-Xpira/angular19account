4263cec969dcc43e3116c345f0134ebf87f4ba05
Alberto Cruz <xpiradesign@gmail.com>
2024-12-09 12:23:08 +0100

    creating first components of feature account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './feature/account/pages/login/login.component';
+import { RegisterComponent } from './feature/account/pages/register/register.component';
+import { ForgetPasswordComponent } from './feature/account/pages/forget-password/forget-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
